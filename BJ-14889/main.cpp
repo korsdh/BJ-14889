42b6b85972c4f0109be3be9a59d1d217679fa3f9
korsdh@naver.com <korsdh@naver.com>
2022-09-16 13:22:32 +0900

    겁나 머리 아프다

--- BJ-14889/main.cpp
+++ BJ-14889/main.cpp
@@ -19,8 +19,24 @@ int team_An = 0;
 int team_Bn = 0;
 vector<pair<int, int>> team_B;
 
+void P(int num) {
+	for (int i = 1; i < A.size(); i++) {
+		if ((num + i) >= A.size()) return;
+		team_A.push_back(make_pair(A[num], A[num + i]));
+		team_B.push_back(make_pair(B[num], B[num + 1]));
+		P(num + 1);
+	}
+	
+}
+
 void make_team(int num) {
 	if (num == (n / 2)) {
+		for (int i = 0; i < n; i++) {
+			if (visit[i] == false) {
+				B.push_back(i);
+			}
+		}
+		P(0);
 		for (int i = 0; i < team_A.size(); i++) {
 			team_An += (ability[team_A[i].first][team_A[i].second] + ability[team_A[i].second][team_A[i].first]);
 			team_Bn += (ability[team_B[i].first][team_B[i].second] + ability[team_B[i].second][team_B[i].first]);
@@ -29,7 +45,10 @@ void make_team(int num) {
 		answer = min(answer, result);
 	}
 	for (int i = 0; i < n;i++) {
-		
+		A.push_back(i);
+		visit[i] = true;
+		make_team(num + 1);
+		visit[i] = false;
 	}
 }
 
